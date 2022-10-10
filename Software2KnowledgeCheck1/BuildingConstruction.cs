5782e7a1638e51afca74aba4e4d8f7b39cafd7af
Alberto Martinez <amartinezmulet@gmail.com>
2022-10-09 22:52:09 -0400

    Add project to git 2

---
+++ Software2KnowledgeCheck1/BuildingConstruction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class BuildingConstruction<T>
+    {
+        public Building? CreateBuilding<T>(T building) where T:Building
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var buildingWasMade = ConstructBuilding<T>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (buildingWasMade)
+            {
+                return building;
+            }
+
+            return null;
+        }
+
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+}
---
+++ Software2KnowledgeCheck1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Software2KnowledgeCheck1;
+
+var city = new City();
+city.Name = "Shively";
+city.State = "Kentucky";
+city.ZipCode = 40216;
+city.Population = 15710;
+
+var apartment1 = new Apartment();
+
+apartment1.StreetNumber = 100;
+apartment1.Street = "Dixie Hwy";
+apartment1.NumberOfOpenUnits = 20;
+apartment1.NumberOfOpenUnits = 10;
+apartment1.HasParking = true;
+
+var apartment2 = new Apartment();
+
+apartment2.StreetNumber = 120;
+apartment2.Street = "Crums Ln";
+apartment2.NumberOfOpenUnits = 25;
+apartment2.NumberOfOpenUnits = 5;
+apartment2.HasParking = true;
+
+
+var higRise1 = new HighRise();
+
+higRise1.Street = "Dixie Hwy";
+higRise1.StreetNumber = 5000;
+higRise1.NumberOfStories = 110;
+higRise1.OwnerName = "John Doe";
+higRise1.Directory = new List<string> { "001- Offices", "002- Apartments", "003- Restaurants" };
+
+
+BuildingConstruction<Apartment> buildingConstruction = new BuildingConstruction<Apartment>();
+
+city.Buildings.Add(buildingConstruction.CreateBuilding<Apartment>(apartment1));
+city.Buildings.Add(buildingConstruction.CreateBuilding<Apartment>(apartment2));
+city.Buildings.Add(buildingConstruction.CreateBuilding<HighRise>(higRise1));
+
+
+
+
+city.Buildings.ForEach((building => 
+{
+    Console.WriteLine($"Address: {building.StreetNumber} {building.Street}");
+}));
+
+Console.ReadLine();
+
+//output
+//     Address: 100 Dixie Hwy
+//     Address: 120 Crums Ln
+//     Address: 5000 Dixie Hwy
